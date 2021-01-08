a805cff7010ec31e69497e884defcd10191feabd
Helge Heß <devteam@zeezide.de>
2021-01-08 17:26:58 +0100

    Add swasi-run
    Just a small tool to run Wasm modules using
    SwiftyWasmer.

--- Package.swift
+++ Package.swift
@@ -6,17 +6,16 @@ let package = Package(
   name: "SwiftyWasmer",
   
   products: [
-    .library(name: "Wasmer", targets: [ "Wasmer" ])
+    .library   (name: "Wasmer",    targets: [ "Wasmer"    ]),
+    .executable(name: "swasi-run", targets: [ "swasi-run" ])
   ],
   
   targets: [
     .systemLibrary(name: "CWasmer", pkgConfig: "wasmer"),
     
-    .target(name         : "Wasmer",
-            dependencies : [ "CWasmer" ],
-            exclude      : [
-              "README.md",
-              "CExtensions/README.md"
-            ])
+    .target(name: "Wasmer", dependencies: [ "CWasmer" ],
+            exclude: [ "README.md", "CExtensions/README.md" ]),
+                
+    .target(name: "swasi-run", dependencies: [ "Wasmer" ])
   ]
 )
